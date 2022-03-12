25ed60186219901a69dd123a60b2351f4783d751
JAYPRAKASH-jp <jauntyjp@gmail.com>
2022-03-12 10:30:45 +0530

    New project

---
+++ Springboot-database-op/Springboot-database-op/src/main/java/com/springboot/Springbootdatabaseop/SpringbootDatabaseOpApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Springbootdatabaseop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatabaseOpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatabaseOpApplication.class, args);
+	}
+
+}
---
+++ Springboot-database-op/Springboot-database-op/src/test/java/com/springboot/Springbootdatabaseop/SpringbootDatabaseOpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.Springbootdatabaseop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatabaseOpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
